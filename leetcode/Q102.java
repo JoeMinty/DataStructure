b4f042c938b8662fa5da2456e1996c1609eaf899
Joe <geekjoechan@gmail.com>
2020-05-05 12:14:26 +0800

    Update Q102.java

--- leetcode/Q102.java
+++ leetcode/Q102.java
@@ -1,28 +1,62 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 二叉树的层次遍历
+ */
 public class Q102 {
+    private List<List<Integer>> res = new ArrayList<>();
+
+    private void dfs(TreeNode node, int depth) {
+        // terminate condition
+        if (node == null) {
+            return;
+        }
+
+        // process
+        while (res.size() <= depth) {
+            res.add(new ArrayList<>());
+        }
+
+        res.get(depth).add(node.val);
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
+    }
+
+    private void bfs(TreeNode node) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> subRes = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+                subRes.add(curr.val);
+            }
+            res.add(subRes);
+        }
+    }
+
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
         if (root == null) {
             return res;
         }
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> subRes = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.remove();
-                subRes.add(node.val);
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
-                }
-            }
-            res.add(subRes);
-        }
+        bfs(root);
+        // dfs(root, 0);
         return res;
+    }
+
+    public static void main(String[] args) {
+        new Q102().levelOrder(new TreeNode(3));
     }
 }
